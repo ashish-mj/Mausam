5674bbbe16cd63df76b4bb364d9662d386f80fbd
Ashish M J <ashm.jagadeesh@gmail.com>
2021-06-18 21:20:37 +0530

    Add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import logo from './images/weather.png';
+import website from './images/website.png';
+import linkdin from './images/linkedin.png';
+import github from './images/github.png';
 
 const api = {
   key : "ad2569b0b91b23602d0a188447c0d986",
@@ -8,13 +12,22 @@ const api = {
 function App() {
   return (
     <div className="app">
-        
+      <section className="weather_app">
+        <div className="nav">
+           <img src={logo} alt="logo" className="logo"/>
+           <p className="name"><b>MAUSAM</b></p>
+           <ul className="links">
+             <li><a href="https://ashishmj.herokuapp.com/"><img src={website} alt="website" className="links_img"/></a></li>
+             <li><a href="https://www.linkedin.com/in/ashish-mj/"><img src={linkdin} alt="linkdin" className="links_img"/></a></li>
+             <li><a href="https://github.com/ashish-mj"><img src={github} alt="github" className="links_img"/></a></li>
+           </ul>
+          </div>
         <main>
-           <h2>MAUSAM</h2>
            <div className="search-box">
              <input type="text" className="search-bar" placeholder="Search"/>
            </div>
         </main>
+      </section>
     </div>
   );
 }
