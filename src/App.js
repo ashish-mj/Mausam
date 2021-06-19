14abeac118998c0033c64e0e26ec26875649dd4d
Ashish M J <ashm.jagadeesh@gmail.com>
2021-06-19 18:36:57 +0530

    Add Styling

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) =>{
+    let months =["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month =months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  }
+
   return (
     <div className="app">
       <section className="weather_app">
@@ -25,6 +38,17 @@ function App() {
         <main>
            <div className="search-box">
              <input type="text" className="search-bar" placeholder="Search..."/>
+           </div>
+           <div className="box">
+           <div className="location-box">
+             <div className="location">New York City</div>
+             <div className="date">{dateBuilder(new Date())}</div>
+           </div>
+
+           <div className="weather-box">
+             <div className="temp">15&#176;c</div>
+             <div className="weather">Sunny</div>
+           </div>
            </div>
         </main>
       </section>
